13b53d682800a70432ebb2a5d3e8c5343120cf65
Eduard Bondarenko <edbond@gmail.com>
2008-07-29 22:19:46 +0300

    hello web / mochiweb example
    Signed-off-by: Eduard Bondarenko <edbond@gmail.com>

---
+++ hello_web.erl
@@ -0,0 +1,12 @@
+-module(hello_web).
+
+-export([start/1, stop/0]).
+
+-define(CONTENT, <<"<html><head><title>Hello, Web</title></head><body>Hello, Web!</body></html>">>).
+
+start(Port) ->
+  mochiweb_http:start([{port, Port}, {loop, fun(Req) ->
+            Req:ok({"text/html", ?CONTENT}) end}]).
+
+stop() ->
+  mochiweb_http:stop().
