ee7c000b3d44911ef614ab04b95d6827548958d9
Eduard Bondarenko <edbond@gmail.com>
2008-10-06 15:37:49 +0300

    more code

--- tproxy/pp2.erl
+++ tproxy/pp2.erl
@@ -15,19 +15,32 @@ handle_info(_Info, _State) -> ok.
 terminate(_Reason, _State) -> ok.
 
 read_until(Stop, Socket, Acc) ->
+  io:format("read_until ~p ~p ~p~n", [Stop, Socket, Acc]),
   {ok, Data} = gen_tcp:recv(Socket, 0),
+  io:format("read ~p~n", [Data]),
+  NewAcc = [Acc | Data],
+  Pos = string:str(binary_to_list(Data), Stop),
+  if
+    Pos == 0 ->
+      io:format("read more...~n", []),
+      read_until(Stop, Socket, NewAcc);
+    true ->
+      io:format("zzzz~n", [])
+  end,
+  io:format("returning ~p~n", [NewAcc]),
+  lists:flatten(NewAcc).
 
 init(Args) ->
   [ClientSocket] = Args,
+  io:format("init: ~p~n", [ClientSocket]),
   Request = read_until("\r\n\r\n", ClientSocket, []),
+  io:format("got request: ~p~n", [Request]),
   {ok, state}.
 
 loop(ListenSocket) ->
-  %Proxy = gen_server:start_link(?MODULE, [], []),
-  %io:format("server process: ~p~n",[Proxy]),
-
   {ok, ClientSocket} = gen_tcp:accept(ListenSocket, infinity),
-  Proxy = gen_server:start_link(?MODULE, [ClientSocket], []),
+  io:format("connection ~p~n", [ClientSocket]),
+  _Proxy = gen_server:start_link(?MODULE, [ClientSocket], []),
 
   loop(ListenSocket).
 
@@ -35,6 +48,7 @@ main() ->
   inets:start(),
   % listen to socket
 
-  {ok, Socket} = gen_tcp:listen(?PORT, [binary, {packet, 0}, {active, once}]),
+  {ok, Socket} = gen_tcp:listen(?PORT, [binary, {packet, 0}, {active, false}]),
+  io:format("listen ~p~n", [Socket]),
 
   loop(Socket).
