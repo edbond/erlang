16d49949367414419e6245c2b8b6b5cce789145c
Eduard Bondarenko <edbond@gmail.com>
2008-10-27 18:40:16 +0200

    mode fix test on

--- tproxy/test.rb
+++ tproxy/test.rb
@@ -7,7 +7,7 @@ require 'net/http'
 proxy_addr = 'localhost'
 proxy_port = 3456
 
-res=Net::HTTP::Proxy(proxy_addr, proxy_port).start('www.google.com') {|http|
+res=Net::HTTP::Proxy(proxy_addr, proxy_port).start('mapia.com.ua') {|http|
 #res=Net::HTTP::Proxy(proxy_addr, proxy_port).start('localhost:3000') {|http|
   http.get('/')
 }
