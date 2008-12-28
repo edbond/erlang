2e55c1de394125e9acd187f5cf999b0313215e57
Eduard Bondarenko <edbond@gmail.com>
2008-12-28 23:06:28 +0200

    makefile, test files, custom iconv

--- fb2/fb2.erl
+++ fb2/fb2.erl
@@ -1,59 +1,60 @@
 -module(fb2).
 -compile(export_all).
--import(erlsom, [parse_sax/3]).
 
-sax_event(ParserFSM, Event, A) ->
-  %io:format("~p~n", [Event]),
-  case Event of
-    startDocument -> ok;
-    endDocument -> ok;
-    {processingInstruction, _Target, _Data} -> ok;
-    {startPrefixMapping, _Prefix , _URI} -> ok;
-    {endPrefixMapping, _Prefix} -> ok;
-    {ignorableWhitespace, _WS} -> ok;
-    {startElement, _Uri, LocalName, _Prefix, _Attributes} ->
-      gen_fsm:sync_send_event(ParserFSM, {element, LocalName});
-    {characters, Chars} ->
-      gen_fsm:sync_send_event(ParserFSM, {characters, Chars});
-    {endElement, _Uri, LocalName, _Prefix} ->
-      gen_fsm:sync_send_event(ParserFSM, {end_element, LocalName});
-    Other ->
-      io:format("unknown event: ~p~n", [Other])
-  end,
-  A.
+-record(fb2_info, {authors=[], title="", annotation=""}).
 
 % convert xml from Encoding to UTF-8
 decode(Encoding, Xml) ->
-  %% substitute <?xml ?> 
-  Xml.
+  case Encoding of
+    "windows-1251" ->
+      io:format("decode: ~s~n", [Encoding]),
+      win1251:decode(Xml);
+    "utf-8" ->
+      Xml
+  end.
 
-parse_fb2(Filename, Acc, Encoding) ->
-  io:format("parse ~p~n", [Encoding]),
-  {ok, Xml}=file:read_file(Filename),
-  if
-    Encoding == "utf-8" ->
-      Utf8Xml = Xml;
-    true ->
-      Utf8Xml = decode(Encoding, Xml)
+extract(Match, String) ->
+  {Start, Length}=lists:nth(2,Match),
+  string:substr(String, Start+1, Length).
+
+cut_re(String, Re, Options, Default) ->
+  case re:run(String, Re, Options) of
+    {match, Match} ->
+      extract(Match, String);
+    nomatch ->
+      Default
+  end.
+
+parse_authors(Xml, Matches, Encoding) ->
+  M=lists:map(fun(X) -> extract(X, Xml) end, Matches),
+  io:format("zz ~w~n", [decode(Encoding, lists:flatten(M) )]).
+
+parse_fb2(Filename, Acc) ->
+  io:format("parse ~p~n", [Filename]),
+  {ok, XmlFile}=file:open(Filename, [read]),
+  {ok, Xml}=file:read(XmlFile, 2048),
+  ok=file:close(XmlFile),
+
+  Fb2Empty=#fb2_info{},
+
+  %% determine encoding
+  Encoding=cut_re(Xml, "<\?.*encoding=\"(.+)\".*/?>", [], "utf-8"),
+  io:format("encoding = ~p~n", [Encoding]),
+
+  TitleInfo=cut_re(Xml, "<title-info>(.+?)</title-info>", [dotall], ""),
+  %io:format("title info: ~p~n", [TitleInfo]),
+
+  %% find <author></author>
+  case re:run(TitleInfo, "<author>(.+?)</author>", [global | [dotall]]) of
+    {match, Authors} ->
+      AuthorsList=parse_authors(TitleInfo, Authors, Encoding);
+    nomatch ->
+      AuthorsList=[]
   end,
 
-  {ok, ParserFSM}=gen_fsm:start_link(fb2_fsm, [], []),
-  try erlsom:parse_sax(Utf8Xml, [], fun(F,A) -> sax_event(ParserFSM, F,A) end)
-  catch {error, Error} ->
-    %% Encoding windows-1251 not supported
-    case re:run(Error, "Encoding (.*) not supported") of
-      nomatch ->
-        io:format("error: ~p~n", [Error]);
-      {match, Captured} ->
-        %% transcode
-        {Start, Length} = lists:last(Captured),
-        NewEncoding = string:substr(Error, Start+1, Length),
-        parse_fb2(Filename, Acc, NewEncoding)
-    end
-  end,
   [Filename]++Acc.
 
 start() ->
-  Results=filelib:fold_files(".", ".*\.fb2$", false, fun(F,A) -> parse_fb2(F, A, "utf-8") end, []),
+  Results=filelib:fold_files(".", ".*\.fb2$", false, fun(F,A) -> parse_fb2(F, A) end, []),
   io:format("results ~p~n", [Results]),
   ok.
